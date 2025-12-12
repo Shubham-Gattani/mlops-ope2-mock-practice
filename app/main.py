9994fa6ab007e2dddaf20a7acf7c673f55703fdc
Shubham-Gattani <21f3002082@ds.study.iitm.ac.in>
2025-12-12 17:20:41 +0000

    Deploy FastAPI fraud service to GKE with Deployment, Service, and HPA

--- app/main.py
+++ app/main.py
@@ -86,4 +86,4 @@ def predict(transaction: Transaction):
 
 # For debugging locally
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8200)
